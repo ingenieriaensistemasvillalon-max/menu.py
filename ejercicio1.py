704cebae52dbd622ab31179135b6592069309e5f
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-11-19 09:15:14 -0600

    Add user input and error handling in ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+# 1. Captura de datos con input()
+nombre = input("Como te llamas?: ")
+print ("Hola",nombre)
+edad= int(input("¿Cuantos años tienes?"))
+print("tendras" edad +1, "años el proximo año")
+
+try:
+numero= int(input("Escribe un numero:"))
+print("El numero es", numero)
+except ValvueError:
+print("Eso no es un numero valido")
