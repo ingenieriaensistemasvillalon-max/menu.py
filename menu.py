555c4a2b81ec3b2eb3c1d12f766c3334650da279
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-11-19 08:56:40 -0600

    Implement main menu function with options

--- menu.py
+++ menu.py
@@ -1,1 +1,22 @@
+def menu():
+    while True:
+        print("\n===== MENÚ PRINCIPAL =====")
+        print("1. Datos del usuario")
+        print("2 Evaluar numero")
+        print("3 Suma y promedio")
+        print("4 Lista de estudiantes")
+        print("5 Área de rectángulo ")
+        print("6 Información del alumno ")
+        print("7  Mostrar todos los datos")
+      print("0. Salir")
+        
+      opcion = input("Elige una opción: ")
 
+        if opcion == "1":
+            ejercicio1()
+        elif opcion == "0":
+          
+            print("Programa finalizado.")
+            break
+        else:
+            print("Opción inválida. Intenta de nuevo.")
