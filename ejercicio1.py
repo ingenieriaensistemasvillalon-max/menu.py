3ead9f0b337d19905da2d536bbf05e6f38f02432
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-11-21 09:13:14 -0600

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,22 @@
 # practica1.py
 
 # 1. Captura de datos con input()
-nombre(input("Ingresa tu nombre: ")
-edad= int(input("¿Cuantos años tienes?"))
-estudias(input("¿Que carrera estudias?"))
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("¿Cuantos años tienes?"))
+estudias = input("¿Que carrera estudias?"))
 
-# 2. Mostrar los datos capturados
+# 2. Conversión de tipos
+nombre = int(nombre)
+edad = int(edad)
+estudias = int(estudias)
+
+# 3. Mostrar los datos capturados
 print("\n--- Datos Capturados ---")
 print("Nombre completo:", nombre)
 print("Edad:", edad)
 print("¿Que carrera estudia?:", estudias)
 
-# 3. Mostrar el tipo de cada variable
+# 4. Mostrar el tipo de cada variable
 print("\n--- Tipos de Datos ---")
 print("Tipo de nombre:", type(nombre))
 print("Tipo de edad:", type(edad))
