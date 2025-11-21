5e60e228012a179364a8920e601712e73c98cdd9
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-11-21 09:04:39 -0600

    Fix input function calls and update comments
    Corrected input function calls and adjusted comments.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,22 +1,18 @@
 # practica1.py
 
 # 1. Captura de datos con input()
-nombre = input("Ingresa tu nombre: ")
+nombre(input("Ingresa tu nombre: ")
 edad= int(input("¿Cuantos años tienes?"))
-estudias= input("¿Que carrera estudias?"))
+estudias(input("¿Que carrera estudias?"))
 
-# 2. Conversión de tipos
-edad = int(edad)
-
-# 3. Mostrar los datos capturados
+# 2. Mostrar los datos capturados
 print("\n--- Datos Capturados ---")
 print("Nombre completo:", nombre)
 print("Edad:", edad)
 print("¿Que carrera estudia?:", estudias)
 
-# 4. Mostrar el tipo de cada variable
+# 3. Mostrar el tipo de cada variable
 print("\n--- Tipos de Datos ---")
 print("Tipo de nombre:", type(nombre))
 print("Tipo de edad:", type(edad))
-print("tendras" edad +1, "años el proximo año")
 print("¿Que carrera estudia?:", estudias)
