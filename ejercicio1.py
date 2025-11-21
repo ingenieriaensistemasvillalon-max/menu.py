c9749d0be564f0941965a8c8aafabbc02e26bf11
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-11-21 09:21:19 -0600

    Correct input handling and type conversion
    Fix input prompts and type conversions for user data.

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,12 +3,13 @@
 # 1. Captura de datos con input()
 nombre = input("Ingresa tu nombre: ")
 edad = int(input("¿Cuantos años tienes?"))
-estudias = input("¿Que carrera estudias?"))
+estudias = input("¿Si estudias? (True/False): ")
+
 
 # 2. Conversión de tipos
 nombre = int(nombre)
 edad = int(edad)
-estudias = int(estudias)
+estudias = estudias == "True"  # Convierte a booleano
 
 # 3. Mostrar los datos capturados
 print("\n--- Datos Capturados ---")
